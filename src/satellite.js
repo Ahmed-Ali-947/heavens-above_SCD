f34f0d07995f4a333b84a928939d43a7fd58c385
Ahmed-Ali-947 <ahmedali947lh@gmail.com>
2025-10-28 23:38:22 +0500

    Fix lint errors in satellite.js

--- src/satellite.js
+++ src/satellite.js
@@ -8,16 +8,16 @@ const events = ["rise", "reachAltitude10deg", "highestPoint", "dropBelowAltitude
 const attribute = ["time", "altitude", "azimuth", "distance", "brightness", "sunAltitude"];
 
 const compare = [
-	function(a, b) {
+	function (a, b) {
 		return a[property[6]][1] >= b[property[6]][1] ? 1 : -1; //星等（越小越好）
 	},
-	function(a, b) {
+	function (a, b) {
 		return a[property[6]][2] >= b[property[6]][2] ? 1 : -1; //太阳高度（越小越好）
 	},
-	function(a, b) {
+	function (a, b) {
 		return a[property[6]][3] <= b[property[6]][3] ? 1 : -1; //卫星高度（越大越好）
 	},
-	function(a, b) {
+	function (a, b) {
 		return a[property[7]] <= b[property[7]] ? 1 : -1; //持续时间（越大越好）
 	}
 ];
@@ -28,6 +28,8 @@ function getTable(config) {
 	let counter = config.counter || 0;
 	const opt = config.opt || 0;
 	const basedir = `${config.root}satellite${config.target}/`;
+	let options;
+
 	if (counter === 0) {
 		options = utils.get_options(`PassSummary.aspx?satid=${config.target}&`);
 		if (!fs.existsSync(basedir)) {
@@ -58,22 +60,24 @@ function getTable(config) {
 		function factory(temp) {
 			return new Promise((resolve, reject) => {
 				request(utils.image_options(temp[property[0]]), (error, response, body) => {
-                    if (error || response.statusCode !== 200) {
+					if (error || response.statusCode !== 200) {
 						reject(error);
 						return;
 					}
-                    console.log("Success", temp);
+					console.log("Success", temp);
 
-                    const $ = cheerio.load(body, {
+					const $ = cheerio.load(body, {
 						decodeEntities: false
 					}); //防止i - shift === 2触发两次的问题
 
-                    const table = $("form").find("table.standardTable");
-                    const tbody = table.find("tbody");
-                    let shift = 0;
+					const table = $("form").find("table.standardTable");
+					const tbody = table.find("tbody");
+					let shift = 0;
 					let flag = false;
 					const data = [];
-                    for (let i = 0; i < tbody.find("tr").length; i++) {
+
+					let current = {};
+					for (let i = 0; i < tbody.find("tr").length; i++) {
 						if (tbody.find("tr").eq(i).find("td").eq(0).text() === "离开地影") { //Exits shadow
 							temp[property[3]][events[5]] = {};
 							current = temp[property[3]][events[5]];
@@ -97,24 +101,24 @@ function getTable(config) {
 							data[3] = parseInt(current[attribute[1]].split("°")[0]);
 						}
 					}
-                    const startTime = utils.getTimestamp(temp[property[3]][events[5]] ? temp[property[3]][events[5]][attribute[0]] : temp[property[3]][events[1]][attribute[0]]);
-                    const endTime = utils.getTimestamp(temp[property[3]][events[6]] ? temp[property[3]][events[6]][attribute[0]] : temp[property[3]][events[3]][attribute[0]]);
-                    temp[property[5]] = "https://www.heavens-above.com/" + $("#ctl00_cph1_imgViewFinder").attr("src") //.replace("size=800", "size=1600");
-                    temp[property[6]] = data;
-                    temp[property[7]] = endTime - startTime;
-                    temp[property[8]] = 0;
-                    const id = utils.md5(Math.random().toString()); //temp[property[1]] + temp[property[7]];
-                    temp[property[9]] = id;
-                    fs.appendFile(basedir + id + ".html", table.html(), (err) => {
+					const startTime = utils.getTimestamp(temp[property[3]][events[5]] ? temp[property[3]][events[5]][attribute[0]] : temp[property[3]][events[1]][attribute[0]]);
+					const endTime = utils.getTimestamp(temp[property[3]][events[6]] ? temp[property[3]][events[6]][attribute[0]] : temp[property[3]][events[3]][attribute[0]]);
+					temp[property[5]] = "https://www.heavens-above.com/" + $("#ctl00_cph1_imgViewFinder").attr("src") //.replace("size=800", "size=1600");
+					temp[property[6]] = data;
+					temp[property[7]] = endTime - startTime;
+					temp[property[8]] = 0;
+					const id = utils.md5(Math.random().toString()); //temp[property[1]] + temp[property[7]];
+					temp[property[9]] = id;
+					fs.appendFile(basedir + id + ".html", table.html(), (err) => {
 						if (err) console.log(err);
 					}); //保存表格
-                    request.get(utils.image_options(temp[property[5]])).pipe(fs.createWriteStream(basedir + id + ".png", {
+					request.get(utils.image_options(temp[property[5]])).pipe(fs.createWriteStream(basedir + id + ".png", {
 						"flags": "a"
 					})).on("error", (err) => {
 						console.error(err);
 					}); //下载图片
-                    resolve(temp);
-                });
+					resolve(temp);
+				});
 			});
 		}
 		Promise.allSettled(queue.map(temp => factory(temp))).then(results => {
@@ -143,7 +147,7 @@ function getTable(config) {
 						return ele;
 					});
 				}
-				database = database.map((ele, index) => {
+				database = database.map((ele, _index) => {
 					if (isNaN(ele[property[6]][1])) {
 						ele[property[8]] = 0;
 						return ele;
